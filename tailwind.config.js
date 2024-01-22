e0fd097f2d44fd30a0b59824d277f81d5e5d3a5e
vitorlinsbinski <vitor.linsbinski@hotmail.com>
2024-01-22 15:14:21 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    extend: {},
+  },
+
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+};
